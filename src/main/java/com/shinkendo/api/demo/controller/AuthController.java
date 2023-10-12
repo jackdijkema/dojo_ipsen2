f4c99214b45a1edf469ea32dc0bd384f9f0492e3
AHsofty <e.basaran2004@gmail.com>
2023-10-12 13:39:03 +0200

    Fix: API endpoints veranderd

--- src/main/java/com/shinkendo/api/demo/controller/AuthController.java
+++ src/main/java/com/shinkendo/api/demo/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/auth")
+@RequestMapping(value = "/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthenticationService authenticationService;
