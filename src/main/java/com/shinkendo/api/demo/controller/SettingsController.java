955acd0bf0cab2ce93006d7fa49fdcd459986799
Freek Weijers <s1137989@student.hsleiden.nl>
2023-11-01 12:38:24 +0100

    fix: models verwijderd

--- src/main/java/com/shinkendo/api/demo/controller/SettingsController.java
+++ src/main/java/com/shinkendo/api/demo/controller/SettingsController.java
@@ -1,10 +1,19 @@
 package com.shinkendo.api.demo.controller;
 
 
-import com.shinkendo.api.demo.dao.SettingsDAO;
+import com.shinkendo.api.demo.dao.UserDAO;
+import com.shinkendo.api.demo.dto.AuthRequestDTO;
+import com.shinkendo.api.demo.dto.UserCreateDTO;
+import com.shinkendo.api.demo.dto.UserResponseDTO;
+import com.shinkendo.api.demo.mapper.UserMapper;
 import com.shinkendo.api.demo.model.ApiResponse;
-import com.shinkendo.api.demo.model.Settings;
+import com.shinkendo.api.demo.model.Role;
+import com.shinkendo.api.demo.model.User;
+import com.shinkendo.api.demo.service.JwtService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -12,25 +21,29 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/api/v1/settings")
-
+@RequiredArgsConstructor
 public class SettingsController {
+    private final UserDAO userDAO;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    SettingsDAO settingsDAO;
-    @GetMapping(value = "{id}")
-    @ResponseBody
-    public ApiResponse<Settings> findByStudentId(@PathVariable UUID id) {
-        Optional<Settings> settings = settingsDAO.findByStudentId(id);
-
-        if (settings.isEmpty()) {
-            return new ApiResponse<>("User's settings not found", HttpStatus.NOT_FOUND);
+    @PreAuthorize("hasAuthority('STUDENT')")
+    @PatchMapping(path = {"/{id}"})
+    public ApiResponse<UserResponseDTO> updatePasswordResponse(@PathVariable("id") UUID id, @RequestBody AuthRequestDTO newpasswordDTO) {
+        Optional<User> foundUser = userDAO.findById(id);
+        if(foundUser.isEmpty()) {
+            return new ApiResponse<>("User not found", HttpStatus.NOT_FOUND);
         }
-        return new ApiResponse<>(settings.get());
+        User user = foundUser.get();
+
+        setPassword(user, newpasswordDTO.getPassword());
+
+        User createdUser = userDAO.create(user);
+        return new ApiResponse<>(userMapper.fromEntity(createdUser));
     }
 
-    @PostMapping
-    @ResponseBody
-    public ApiResponse<Settings> save(@RequestBody Settings settings) {
-        return new ApiResponse<>(settingsDAO.save(settings), HttpStatus.ACCEPTED);
+    private void setPassword(User user, String password){
+        user.setPassword(passwordEncoder.encode(password));
     }
 
 }
--- src/main/java/com/shinkendo/api/demo/repository/SettingsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shinkendo.api.demo.repository;
-
-import com.shinkendo.api.demo.model.Settings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SettingsRepository extends JpaRepository<Settings, UUID> {
-}
