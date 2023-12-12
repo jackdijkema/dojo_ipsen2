d890781f047cb9eadf5f9c910c265a656d7b201c
Freek Weijers <s1137989@student.hsleiden.nl>
2023-12-12 15:38:50 +0100

    fix: findall en save

--- src/main/java/com/shinkendo/api/demo/dao/ProgressReviewDAO.java
+++ src/main/java/com/shinkendo/api/demo/dao/ProgressReviewDAO.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ProgressReviewDAO {
 
-    ProgressReviewRepository progressReviewRepository;
+    private final ProgressReviewRepository progressReviewRepository;
 
     public List<ProgressReview> findAll() {return progressReviewRepository.findAll();}
 
