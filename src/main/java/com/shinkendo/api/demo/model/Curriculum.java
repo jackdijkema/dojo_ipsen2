3f4432297a9c9b98cd7f4ef10f3d7d256a2efc9f
pk1260 <pascalkuiper@live.nl>
2024-01-19 17:03:40 +0100

    Added @Override toString function to prevent StackOverFlow error for users seeing content

--- src/main/java/com/shinkendo/api/demo/model/Curriculum.java
+++ src/main/java/com/shinkendo/api/demo/model/Curriculum.java
@@ -3,10 +3,7 @@ package com.shinkendo.api.demo.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -35,4 +32,12 @@ public class Curriculum {
         return id.hashCode();
     }
 
+    @Override
+    public String toString() {
+        return "Curriculum{" +
+                "id=" + id +
+                ", rank=" + (rank != null ? rank.getId() : null) + // Extracting only the ID to avoid circular reference
+                ", techniques=" + techniques +
+                '}';
+    }
 }
--- src/main/java/com/shinkendo/api/demo/model/Rank.java
+++ src/main/java/com/shinkendo/api/demo/model/Rank.java
@@ -36,4 +36,12 @@ public class Rank {
         return id.hashCode();
     }
 
+    @Override
+    public String toString() {
+        return "Rank{" +
+                "id=" + id +
+                ", orderId=" + orderId +
+                ", rankName='" + rankName + '\'' +
+                '}';
+    }
 }
