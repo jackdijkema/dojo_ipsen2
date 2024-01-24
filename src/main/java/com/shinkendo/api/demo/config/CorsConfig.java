297b1dac8874f9bd9d77dc6ac8268432d345a054
wouter <wouter@debruijn.dev>
2024-01-24 12:21:04 +0100

    chore: fix cors

--- src/main/java/com/shinkendo/api/demo/config/CorsConfig.java
+++ src/main/java/com/shinkendo/api/demo/config/CorsConfig.java
@@ -1,16 +1,21 @@
 package com.shinkendo.api.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("allowedOrigins: " + allowedOrigins);
+
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://shinkendo.wouter.cloud/")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
